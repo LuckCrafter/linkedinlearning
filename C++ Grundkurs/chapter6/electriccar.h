bb2f9d027a455d30fe21a226c05b22b98846c9c5
Jeremy Gottlieb <jeremy@gottlieb.cyou>
2024-10-25 19:59:03 +0200

    typo

--- C++ Grundkurs/chapter6/electriccar.h
+++ C++ Grundkurs/chapter6/electriccar.h
@@ -7,7 +7,7 @@ class ElectricCar : public PKW {
     public:
         void accelerate(const float seconds) override;
 
-        ElectricCar;
+        ElectricCar();
 
     private:
         const float mAcceleration = 20000.0f; // km/(h^2)  
