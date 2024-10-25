bd8603c21d3639cca5043d05c46aacf8da72edc4
Jeremy Gottlieb <jeremy@gottlieb.cyou>
2024-10-25 11:08:31 +0200

    add challange from chapter 3

--- cppLernen.cpp
+++ cppLernen.cpp
@@ -2,6 +2,29 @@
 #include <ctime>
 #include <string>
 
+/*      Kapitel 3       */
+
+/*
+Challange 1:
+1. Legen Sie ein Array mit 5 Werten fest.
+2. Weisen Sie jedem Wert im Array den 3-fachen Wert des Index zu.
+3. Geben Sie die Anzahl der Werte aus, die größer gleich 6 sind.
+*/
+void kapitel3() {
+    int myArray[5];
+    for(int i=0;i<5;++i) {
+        myArray[i]=i*3;
+    }
+    int counter {0};
+    for(auto e : myArray) {
+        if (e>=6) ++counter;
+    }
+    std::cout << counter;
+}
+
+
+/*      Kapitiel 4      */
+
 /*
 Challenge 1: Void Fubktion
 1. Erstellen Sie eine void-Funktion mit dem Namen ausgabeMeinArray, die ein array als Parameter übernimmt.
@@ -60,6 +83,6 @@ void schereSteinPapier() {
 }
 
 int main() {
-    schereSteinPapier();
+    kapitel3();
     return 0;
 }
