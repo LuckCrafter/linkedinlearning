94ce7cd2a4bc8bad842056b1a6518de9bfe9ade9
LuckCrafter <jeremy@gottlieb.cyou>
2024-10-30 15:09:42 +0100

    use std::thread

---
+++ C++ Grundkurs 2/Chapter 3/thread.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <thread>
+
+int main() {
+    std::thread t1( [&] {
+        std::cout << "1. Lock." << std::endl;
+        std::cout << "1. My turn!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "1. Still my turn!" << std::endl;
+        std::cout << "1. Release" << std::endl;
+    });
+
+    std::thread t2( [&] {
+        std::cout << "2. Lock." << std::endl;
+        std::cout << "2. My turn!" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "2. Still my turn!" << std::endl;
+        std::cout << "2. Release" << std::endl;
+    });
+
+    t1.join();
+    t2.join();
+    return 0;
+}
