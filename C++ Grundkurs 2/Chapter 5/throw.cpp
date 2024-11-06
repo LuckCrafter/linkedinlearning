5b1050b95683fef256564781e626279ec8a96b45
LuckCrafter <jeremy@gottlieb.cyou>
2024-11-06 15:18:47 +0100

    own throws inside try catch blocks

---
+++ C++ Grundkurs 2/Chapter 5/throw.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <exception>
+
+int main(){
+
+    std::vector<std::string> data;
+
+    try {
+        data.push_back("Test");
+        data.push_back("Test2");
+
+        if(data.size() < 10) throw "Sorry, your array is too short.";
+        
+        std::cout << data.at(10) << std::endl;
+        
+        data.push_back("Test3");
+        data.push_back("Test5");
+    } catch(const char *str) {
+        std::cout << str << std::endl;
+        data.push_back("Test after exception thrown with str"); 
+    } catch(const std::exception &e) {
+            std::cout << e.what() << std::endl;
+            data.push_back("Test after exception");
+    } catch(...) {
+        std::cout << "Unknown Exception caught!\n";
+    }
+
+    for(const auto e : data)
+      std::cout << e << std::endl;
+    return 0;
+}
