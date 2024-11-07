783b23009acfcea2641c6dbef298949bfacb8864
LuckCrafter <jeremy@gottlieb.cyou>
2024-11-07 10:36:48 +0100

    init C++Tipps und Tricks (span)

---
+++ C++ Tipps und Tricks/spanning.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <list>
+#include <span>
+
+void printC(std::span<int> container) {
+    for(const auto& e : container)
+        std::cout << e << " ";
+    std::cout << std::endl;
+    return;
+}
+
+int main(){
+    int arr1 [] {1,2,3,4,5,6,7,8};
+    std::array<int, 8> arr2 {1,2,3,4,5,6,7,8};
+    std::vector<int> arr3 {1,2,3,4,5,6,7,8};
+    //std::list<int> arr4 {1,2,3,4,5,6,7,8};
+
+    printC(arr1);
+    printC(arr2);
+    printC(arr3);
+    //printC(arr4); // list dont work
+
+    return 0;
+}
