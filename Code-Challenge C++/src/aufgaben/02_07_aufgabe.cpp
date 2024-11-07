eaec93b391492e8ea4a17f44cced445594a7add1
LuckCrafter <jeremy@gottlieb.cyou>
2024-11-07 15:52:37 +0100

    solve 07

--- Code-Challenge C++/src/aufgaben/02_07_aufgabe.cpp
+++ Code-Challenge C++/src/aufgaben/02_07_aufgabe.cpp
@@ -3,7 +3,10 @@
 int fibonacci(int n)
 {
     // todo implementieren Sie die Fibonacci Folge
-    return 0;
+    if (n==0) return 0;
+    if (n==1) return 1;
+
+    return fibonacci(n-2)+fibonacci(n-1);
 }
 
 int main()
