c69fe404a8da0e68df143535a19dbc4dece91d6e
LuckCrafter <jeremy@gottlieb.cyou>
2024-11-13 11:26:09 +0100

    solve 12

--- Code-Challenge C++/src/aufgaben/02_12_aufgabe.cpp
+++ Code-Challenge C++/src/aufgaben/02_12_aufgabe.cpp
@@ -3,17 +3,55 @@
 
 class Tier
 {
+protected:
+    int m_alter;
 public:
-    Tier(){};
+    Tier(){m_alter = 10;};
     virtual ~Tier(){};
 
     void ausgabeTier() { std::cout << "Ich bin ein Tier." << std::endl; }
 
     virtual void machGerausch() = 0;
+    virtual int getAlter() {return m_alter;}
+};
+
+class Maus : public Tier {
+public:
+    void machGerausch() override {
+        std::cout << "pieps.\n";
+    }
+};
+class Hund : public Tier {
+public:
+    void machGerausch() override {
+        std::cout << "wuff.\n";
+    }
+};
+class Katze : public Tier {
+public:
+    void virtual machGerausch() override {
+        std::cout << "miau.\n";
+    }
 };
 
 int main()
 {
+    
 
+    {
+        Maus obj;
+        std::cout << obj.getAlter();
+        obj.machGerausch();
+    }
+    {
+        Hund obj;
+        std::cout << obj.getAlter();
+        obj.machGerausch();
+    }
+    {
+        Katze obj;
+        std::cout << obj.getAlter();
+        obj.machGerausch();
+    }
     return 0;
 }
